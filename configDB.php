51cc3f8eb0fa3c050141643b04f4480e99c2d3a0
mutatolaurent <mutato.laurent@gmail.com>
2026-03-04 11:03:28 +0100

    V1.2 : exclusion configDB.php du tracking Git

--- configDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Déclaration des constantes pour la connection à la base de données
-define('DB_NAME', 'carnet');
-define('DB_HOST', 'localhost');
-define('DB_PORT', '3306');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_CHARSET', 'utf8mb4');
